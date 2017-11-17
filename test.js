ad96ad05c8c4f1d5119d726321d6c92e44c3a8fa
gr4per <arne.graeper@opuscapita.com>
2017-11-17 10:42:19 +0000

    now consul is tunneled to local 8500

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
+var dns = require('dns');
 var net = require('net');
 var Client = require('ssh2').Client;
 var axios = require('axios');
@@ -10,36 +11,76 @@ var ssh_config = {
   agent: process.env.SSH_AUTH_SOCK
 }
 
-var srv = net.createServer( (conn) =>  {
-  console.log("socket accept handler, creating new ssh connection to forward traffic");
-  conn.on('end', () => {
-    console.log('client disconnected');
-  });
+var mysqlHostname = 'dev-mysql0';
+var mysqlStream;
+var mysqlConn;
+var mysqlServer;
 
-  var sshconn = new Client();
-  sshconn.on('ready', function() {
-    sshconn.forwardOut('localhost',
-                    8500,
-                    '10.26.19.197',
-                    8500,
-                    function(err, stream) {
-      if (err) {
-        console.out("forwarding failed: " + err);
-        sshconn.end();
-      }
-      else {
-        console.log("forwarding via ssh, piping stream to socket");
-        stream.pipe(conn);
-        conn.pipe(stream);
-        sshconn.on('close', function() {
-          conn.end();
-        }); 
-      }
-    });
-  });
+var sshconn = new Client();
+sshconn.on('ready', function() {
+  console.log("ssh connection to " + ssh_config.host + " established");
+});
+var mysqlIp = dns.lookup(ssh_config.host, function() {
+  console.log('looked up ' + ssh_config.host + " to be " + mysqlIp);
   sshconn.connect(ssh_config);
 });
 
+var srv = net.createServer( (conn) =>  {
+  console.log("handling client request, remote port = " + conn.remotePort);
+  if(mysqlConn) {
+    console.log("rejecting, mysql conn already in use");
+    conn.end();
+    return;
+  }
+  conn.on('end', () => {
+    console.log('client disconnectedi from socket, unpiping stream');
+    mysqlStream.unpipe();
+    mysqlStream.close();
+    if(mysqlConn) mysqlConn.unpipe();
+    mysqlConn = null;
+    console.log("unpipe done");
+  });
+  conn.on('close', () => {
+    console.log('client socket closed');
+    mysqlStream.unpipe();
+    if(mysqlConn) mysqlConn.unpipe();
+    mysqlConn = null;
+  });
+
+  
+  console.log("connecting client to mysqlStream, remote address " + conn.remoteAddress + ", remote port " + conn.remotePort + ", stream " + mysqlStream);
+
+  sshconn.forwardOut('localhost',
+                  8500,
+                  '10.26.19.197',
+                  8500,
+                  function(err, stream) {
+    if (err) {
+      console.out("forwarding failed: " + err);
+      sshconn.end();
+    }
+    else {
+      console.log("forwarding via ssh, piping stream to socket");
+      mysqlStream = stream;
+      stream.on('end', function(msg) {
+        console.log('stream end event on mysqlStream ' + msg);
+      });
+      sshconn.on('close', function() {
+        console.log("sshconn stream closing");
+        mysqlStream = null;
+        if(mysqlConn)
+          mysqlConn.end();
+      });
+      mysqlStream.pipe(conn);
+      conn.pipe(mysqlStream);
+
+    }
+
+  });
+  mysqlConn = conn;
+  console.log("client " + conn.remotePort + " attached to mysqlStream");
+});
+srv.maxConnections = 1;
 srv.on('error', (err) => {
   console.log("server error : " + err);
 });
@@ -47,16 +88,23 @@ srv.on('error', (err) => {
 var serverPort= 8500;
 srv.listen(serverPort, () => {
   console.log('server bound to ' + serverPort);
-  doSomething();
+  //doSomething();
 });
 
 function doSomething() {
-  axios.get('http://localhost:8500/v1/catalog/services')
-  .then((response) => {
-    console.log(response.data);
-  })
-  .catch(error => {
-    console.log(error);
-  });
+  if(mysqlStream) {
+    console.log("doing something with axios");
+    axios.get('http://localhost:8500/v1/catalog/services')
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  }
+  else {
+    console.log("mysqlStream not ready yet, waiting");
+    setTimeout(doSomething, 100);
+  }
 }
 console.log('hello world!');
