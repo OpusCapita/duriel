32ec223471143f638c7ac08a532fcccbd4f900d5
gr4per <arne.graeper@opuscapita.com>
2017-11-17 12:01:51 +0000

    consul and mysql now working

--- test.js
+++ test.js
@@ -12,84 +12,143 @@ var ssh_config = {
 }
 
 var mysqlHostname = 'dev-mysql0';
-var mysqlStream;
-var mysqlConn;
-var mysqlServer;
+var mysqlIp;
+
+var consulStream;
+var consulData = '';
 
 var sshconn = new Client();
 sshconn.on('ready', function() {
   console.log("ssh connection to " + ssh_config.host + " established");
+  connectConsul();
 });
-var mysqlIp = dns.lookup(ssh_config.host, function() {
-  console.log('looked up ' + ssh_config.host + " to be " + mysqlIp);
-  sshconn.connect(ssh_config);
-});
+sshconn.connect(ssh_config);
 
-var srv = net.createServer( (conn) =>  {
-  console.log("handling client request, remote port = " + conn.remotePort);
-  if(mysqlConn) {
-    console.log("rejecting, mysql conn already in use");
-    conn.end();
-    return;
+function queryConsul(apiCall, callback) {
+  if(!consulStream) {
+    setTimeout(function() { queryConsul(apiCall, callback); }, 100);
+    console.log("retrying query later");
   }
-  conn.on('end', () => {
-    console.log('client disconnectedi from socket, unpiping stream');
-    mysqlStream.unpipe();
-    mysqlStream.close();
-    if(mysqlConn) mysqlConn.unpipe();
-    mysqlConn = null;
-    console.log("unpipe done");
+  else { 
+    consulStream.on('end', () => {
+      callback();
+      consulData = '';
+    });
+    consulStream.write('GET ' + apiCall + ' HTTP/1.1\nHost: localhost\n\n');
+  }
+}
+
+function lookupMysql() {
+  axios.get('http://localhost:' + consulPort + '/v1/catalog/service/mysql')
+  .then((response) => {
+    console.log('mysql looked up: ' + JSON.stringify(response.data));
+    mysqlIp = response.data[0].Address;
+    console.log('mysqlIp = ' + mysqlIp);
+    connectMysql();
+  })
+  .catch(error => {
+    console.log("error making http call to tunneled consul");
+    console.log(error);
   });
-  conn.on('close', () => {
-    console.log('client socket closed');
-    mysqlStream.unpipe();
-    if(mysqlConn) mysqlConn.unpipe();
-    mysqlConn = null;
+}
+
+var mysqlSrv;
+var mysqlPort;
+function connectMysql() {
+  mysqlSrv = net.createServer( (conn) =>  {
+    console.log("handling client request, remote port = " + conn.remotePort);
+    conn.on('end', () => {
+      console.log('mysql client disconnected from socket');
+    });
+    conn.on('close', () => {
+      console.log('mysql client socket closed');
+    });
+    
+    console.log("connecting client to mysqlStream, remote address " + conn.remoteAddress + ", remote port " + conn.remotePort);
+
+    sshconn.forwardOut('localhost',
+		    3306,
+		    mysqlIp,
+		    3306,
+		    function(err, stream) {
+      if (err) {
+	console.out("mysql forwarding failed: " + err);
+	sshconn.end();
+      }
+      else {
+	console.log("forwarding via ssh, piping stream to socket");
+	stream.on('end', function(msg) {
+	  console.log('stream end event on mysqlStream ' + msg);
+	});
+	sshconn.on('close', function() {
+	  console.log("sshconn stream closing");
+	});
+	stream.pipe(conn);
+	conn.pipe(stream);
+      }
+    });
+    console.log("mysql client " + conn.remotePort + " attached to mysqlStream");
+  });
+  mysqlSrv.maxConnections = 1;
+  mysqlSrv.on('error', (err) => {
+    console.log("mysql server error : " + err);
   });
 
-  
-  console.log("connecting client to mysqlStream, remote address " + conn.remoteAddress + ", remote port " + conn.remotePort + ", stream " + mysqlStream);
-
-  sshconn.forwardOut('localhost',
-                  8500,
-                  '10.26.19.197',
-                  8500,
-                  function(err, stream) {
-    if (err) {
-      console.out("forwarding failed: " + err);
-      sshconn.end();
-    }
-    else {
-      console.log("forwarding via ssh, piping stream to socket");
-      mysqlStream = stream;
-      stream.on('end', function(msg) {
-        console.log('stream end event on mysqlStream ' + msg);
-      });
-      sshconn.on('close', function() {
-        console.log("sshconn stream closing");
-        mysqlStream = null;
-        if(mysqlConn)
-          mysqlConn.end();
-      });
-      mysqlStream.pipe(conn);
-      conn.pipe(mysqlStream);
-
-    }
-
+  mysqlSrv.listen(0, () => {
+    mysqlPort = mysqlSrv.address().port;
+    console.log('mysql server bound to ' + mysqlPort);
+    //doSomething();
   });
-  mysqlConn = conn;
-  console.log("client " + conn.remotePort + " attached to mysqlStream");
-});
-srv.maxConnections = 1;
-srv.on('error', (err) => {
-  console.log("server error : " + err);
-});
 
-var serverPort= 8500;
-srv.listen(serverPort, () => {
-  console.log('server bound to ' + serverPort);
-  //doSomething();
-});
+}
+
+var consulSrv;
+var consulPort;
+function connectConsul() {
+  consulSrv = net.createServer( (conn) =>  {
+    console.log("consulSrv handling client request, remote port = " + conn.remotePort);
+    conn.on('end', () => {
+      console.log('consul client disconnected from socket');
+    });
+    conn.on('close', () => {
+      console.log('consul client socket closed');
+    });
+
+    console.log("connecting client to consulStream, remote address " + conn.remoteAddress + ", remote port " + conn.remotePort);
+
+    sshconn.forwardOut('localhost',
+		    consulPort,
+		    'localhost',
+		    8500,
+		    function(err, stream) {
+      if (err) {
+	console.log("forwarding failed: " + err);
+	sshconn.end();
+      }
+      else {
+	console.log("consul forwarding via ssh, piping stream to socket");
+	stream.on('end', function(msg) {
+	  console.log('stream end event on consulStream ' + msg);
+	});
+	sshconn.on('close', function() {
+	  console.log("sshconn stream closing");
+	});
+	stream.pipe(conn);
+	conn.pipe(stream);
+      }
+    });
+    console.log("client " + conn.remotePort + " attached to consulStream");
+  });
+  consulSrv.maxConnections = 1;
+  consulSrv.on('error', (err) => {
+    console.log("consul server error : " + err);
+  });
+  consulSrv.listen(0, () => {
+    consulPort = consulSrv.address().port;
+    console.log('consul server bound to ' + consulPort + ", trying to lookup mysql...");
+    lookupMysql();
+  });
+}
 
 function doSomething() {
   if(mysqlStream) {
@@ -105,6 +164,7 @@ function doSomething() {
   else {
     console.log("mysqlStream not ready yet, waiting");
     setTimeout(doSomething, 100);
-  }
+  
+}
 }
 console.log('hello world!');
