fea39913d9e5d27bebaf9fffe9062454e156db8a
Stefan Turbine <Stefan.Tuebben@yahoo.de>
2018-03-19 15:55:48 +0100

    -kill process on error in buildscripts

--- buildscript.js
+++ buildscript.js
@@ -25,40 +25,45 @@ const pushDockerImage = require('./actions/pushDockerImage');
 const saveObject2File = require('./actions/saveObject2File');
 
 const execute = async () => {
-    // Preparing
-    const config = getEnvVariables();
-    config['REPO_PATH'] = calculateRepoPath(config);
-    config['TARGET_ENV'] = calculateTargetEnv(config);
-    config['MYSQL_PWD'] = `SECRET_${config['TARGET_ENV']}_MYSQL`;
-    config["VERSION"] = calculateVersion(config);
-
-    //Building, Starting, Testing locally
-    await dockerLogin(config);
-    const compose_base = getComposeCommand();
-    await dockerCompose(compose_base, "pull");
-    await buildDockerImage(config);
-    await dockerCompose(compose_base, "up -d");
-
     try {
-        await monitorDockerContainer(config['CIRCLE_PROJECT_REPONAME'], 20, 5000);    // 20 attempts with 5 sec intervals
-    } catch (error){
-        log.error(JSON.stringify(error));
-        await outputContainerLogs();
+        // Preparing
+        const config = getEnvVariables();
+        config['REPO_PATH'] = calculateRepoPath(config);
+        config['TARGET_ENV'] = calculateTargetEnv(config);
+        config['MYSQL_PWD'] = `SECRET_${config['TARGET_ENV']}_MYSQL`;
+        config["VERSION"] = calculateVersion(config);
+
+        //Building, Starting, Testing locally
+        await dockerLogin(config);
+        const compose_base = getComposeCommand();
+        await dockerCompose(compose_base, "pull");
+        await buildDockerImage(config);
+        await dockerCompose(compose_base, "up -d");
+
+        try {
+            await monitorDockerContainer(config['CIRCLE_PROJECT_REPONAME'], 20, 5000);    // 20 attempts with 5 sec intervals
+        } catch (error) {
+            log.error(JSON.stringify(error));
+            await outputContainerLogs();
+            process.exit(1);
+        }
+        await runUnitTests(compose_base);
+        await setGitCredentials(config);
+        await tagGitCommit(config['VERSION'], config['CIRCLE_SHA1']);
+
+        // Starting Deployment
+        if (config['TARGET_ENV'] === 'none') {
+            log.info("no target-environment associated with the branch \n no deployment is going to happen. \n exiting.");
+            process.exit(0);
+        }
+
+        await pushDockerImage(config['HUB_REPO'], ["latest", config['VERSION']], ["latest"]);
+        log.info("saving config for later buildprocess-steps");
+        saveObject2File(config, "bp-config.json", true);
+    }catch (error){
+        log.error(error);
         process.exit(1);
     }
-    await runUnitTests(compose_base);
-    await setGitCredentials(config);
-    await tagGitCommit(config['VERSION'], config['CIRCLE_SHA1']);
-
-    // Starting Deployment
-    if(config['TARGET_ENV'] === 'none'){
-        log.info("no target-environment associated with the branch \n no deployment is going to happen. \n exiting.");
-        process.exit(0);
-    }
-
-    await pushDockerImage(config['HUB_REPO'], ["latest", config['VERSION']], ["latest"]);
-    log.info("saving config for later buildprocess-steps");
-    saveObject2File(config, "bp-config.json", true);
 };
 execute();
 
