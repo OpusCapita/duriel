dc8d01537af1d9e4defb4cf907d59261ab9bcac4
Stefan Turbine <Stefan.Tuebben@yahoo.de>
2018-03-28 17:04:02 +0200

    - better logs are better logs

--- actions/tagAndPushDockerImage.js
+++ actions/tagAndPushDockerImage.js
@@ -7,11 +7,11 @@ module.exports = async function (hub_repo, src_tag, target_tag, push_tag) {
     log.info("pushing to docker...");
     const proxy = new EnvProxy();
     if(src_tag && target_tag) {
-        log.info(`adding tag ${target_tag} to tag ${src_tag}`);
+        log.info(`adding tag '${target_tag}' to tag '${hub_repo}:${src_tag}'`);
         await proxy.executeCommand_L(`docker tag ${hub_repo}:${src_tag} ${hub_repo}:${target_tag}`);
     }
     if(push_tag) {
-        log.info(`pushing tag ${push_tag}...`);
+        log.info(`pushing tag '${push_tag}' to dockerhub...`);
         await proxy.executeCommand_L(`docker push ${hub_repo}:${push_tag}`);
         log.info("...finished pushing docker image")
     }
