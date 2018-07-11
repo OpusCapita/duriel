b05a841d2ddcbafc5d537e40b1bf1b3a804c4e72
Stefan Turbine <Stefan.Tuebben@yahoo.de>
2018-07-11 14:53:17 +0200

    fixed hostname

--- actions/cleanupSystem.js
+++ actions/cleanupSystem.js
@@ -1,26 +1,23 @@
 'use strict';
 const Logger = require('../EpicLogger');
 const log = new Logger();
-module.exports = executeCleanup;
-
-
-async function executeCleanup(proxy, config) {
+module.exports = async function executeCleanup(proxy, config) {
     const env = config['TARGET_ENV'];
     log.info("Cleaning up systen after deployment on: " + env);
     const nodes = await proxy.getNodes_E();
     config['cleanups'] = config['cleanups'] ? config['cleanups'] : {};
     const entries = [];
     for (const node of nodes) {
-        await proxy.executeCommand_N(node.node, "docker system prune -f")
+        await proxy.executeCommand_N(node.hostname, "docker system prune -f")
             .then(response => {
-                log.debug(`cleaned node '${node.node}': `, response);
+                log.debug(`cleaned node '${node.hostname}': `, response);
                 const entry = {
-                    node: node.node,
+                    node: node.hostname,
                     result: response
                 };
                 entries.push(entry);
             })
-            .catch(error => log.warn(`could not prune node '${node.node}'`, error));
+            .catch(error => log.warn(`could not prune node '${node.hostname}'`, error));
     }
     config['cleanups'][env] = entries;
 }
