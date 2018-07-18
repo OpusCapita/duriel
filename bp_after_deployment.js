a05721094cb8a08f03e2d1fcbd1d47790f4ad35e
StefanTuebben <stefan.tuebben@yahoo.de>
2018-07-18 21:50:51 +0200

    update version in dev and master?

--- bp_after_deployment.js
+++ bp_after_deployment.js
@@ -73,7 +73,8 @@ async function handleProductionDeployment(config) {
     const compose_base = dockerCommandBuilder.dockerComposeBase();
     await buildDocs(compose_base, config, true);
     await gitHelper.setCredentials(config['GIT_USER'], config['GIT_EMAIL']);
-    await versionHandler.bumpAndCommitVersionFile(undefined, undefined, undefined); // undefined, undefined, undefined --> load the file, bump as 'patch', ${version} [ci skip] message
+    await versionHandler.bumpAndCommitVersionFile(undefined, undefined, undefined, "master");
+    await versionHandler.bumpAndCommitVersionFile(undefined, undefined, undefined, "develop");
 }
 
 
