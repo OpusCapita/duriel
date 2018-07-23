80e8af1df183495cb57c16b9347e750c01e085d4
Stefan Turbine <Stefan.Tuebben@yahoo.de>
2018-07-23 12:27:50 +0200

    enhanced libVersionFetcher: - added function to fetch multiple envs with one run - added packaging of results into tar-file - added info into README

--- libVersionFetcher.js
+++ libVersionFetcher.js
@@ -8,13 +8,39 @@ const fs = require('fs');
 
 
 async function exec() {
-    if(process.argv.length < 4){
-        log.error(`usage: 'node libVersionFetcher.js {env} {user}'`);
+    if (process.argv.length < 4) {
+        log.error(`usage: 'node libVersionFetcher.js {user} {env} [{env}] '`);
         return;
     }
+    const targetEnvs = process.argv.slice(3);
+    let fetchedData = {};
+    for (const targetEnv of targetEnvs) {
+        const data = await fetchLibVersions(targetEnv)
+            .catch(error => log.error("error during lib-fetching: ", error));
+        if (data) {
+            fetchedData[targetEnv] = data;
+        }
+    }
+    const dir = `${new Date().getTime()}_libs`;
+    fs.mkdirSync(dir);
 
-    const targetEnv = process.argv[2];
-    const user = process.argv[3];
+    for (const env in fetchedData) {
+        const filePath = `${dir}/${env}.csv`;
+        log.info(` Saving information for '${env}' into file '${filePath}'... `);
+        let content = "service;web-init;event-client\n" + fetchedData[env].sort().join('\n');
+        fs.writeFileSync(`./${filePath}`, content);
+        log.info(`Successfully exported library-infos into file: ${filePath}`);
+    }
+
+    log.info("packaging results...")
+    const utilProxy = new EnvProxy();
+    utilProxy.executeCommand_L(`tar -czf ${dir}.tar ${dir}/`);
+    log.info(`finished packaging. file: '${dir}.tar'`)
+
+}
+
+async function fetchLibVersions(targetEnv) {
+    const user = process.argv[2];
     const connectionData = envInfo[targetEnv];
     connectionData.admin_user = user;
 
@@ -22,12 +48,11 @@ async function exec() {
     const proxy = await new EnvProxy().init(connectionData);
 
     const nodes = await proxy.getNodes_E();
-
     const data = [];
-
     for (const node of nodes) {
         log.info(`Fetching data from node: ${node.hostname}`);
-        const containers = await proxy.getContainers_N(node.hostname, true);
+        const containers = await
+            proxy.getContainers_N(node.hostname, true);
         for (const container of containers) {
             log.info(`\tfetching container ${container.containerId}`);
             const command = `docker exec -t ${container.containerId} cat package.json`;
@@ -39,19 +64,12 @@ async function exec() {
                 const eventClientVersion = parsedPackageJSON.dependencies['@opuscapita/event-client'];
                 data.push(`${serviceName};${webInitVersion ? webInitVersion : ""};${eventClientVersion ? eventClientVersion : ""};`);
             } catch (e) {
-
+                // if the containers does not contain a package.json, it is not needed.
             }
         }
     }
-
-
-    const fileName = `libs_${targetEnv}_${new Date().getTime()}.csv`;
-    log.info("Saving information into file... ");
-    let content = "node;service;web-init;event-client\n" + data.join('\n');
-    fs.writeFileSync(`./${fileName}`, content);
-    log.info(`Successfully exported library-infos into file: ${fileName}`);
-
     proxy.close();
+    return data;
 }
 
 exec();
