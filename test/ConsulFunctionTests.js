677a4b85a6ce054ddfe8ab48e5b29ed1aa18fb8e
Stefan Tübben <StefanTurbine@googlemail.com>
2018-08-23 13:29:37 +0200

    removed last connection info.

--- test/ConsulFunctionTests.js
+++ test/ConsulFunctionTests.js
@@ -16,7 +16,7 @@ async function run() {
         let proxy;
         before(async () => {
             const proxyConfig = require('../envInfo').develop;
-            proxyConfig.admin_user = 'tubbest1';
+            //proxyConfig.admin_user = 'tubbest1';
             proxy = await new EnvProxy().init(proxyConfig)
                 .catch(e => log.error(e))
         });
