55d3e25562aa447d7f54eabbdee474c0878967b2
Stefan Tübben <StefanTurbine@googlemail.com>
2018-09-04 15:19:20 +0200

    [ci skip]

--- test/TestSuite.js
+++ test/TestSuite.js
@@ -9,7 +9,6 @@ process.env.andariel_loglevel = "warn";
 
 async function fun() {
     describe("Test duriel!", async () => {
-        /*
         await require("./CommandBuilderTests").run();
         await require("./FileHandlingsTests").run();
         await require("./VersionTests").run();
@@ -20,7 +19,6 @@ async function fun() {
         await require("./VariableInjectionTests").run();
         await require("./LibraryTests").run();
         await require('./ConsulFunctionTests').run();
-        */
     })
 }
 
