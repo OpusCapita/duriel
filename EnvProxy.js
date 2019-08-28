ece75c36f3ed5263a3489bc50c10b4ac8be2eee7
Stefan Saftic <stefan.saftic@gmail.com>
2019-08-28 10:52:59 +0200

    debug ssh init

--- EnvProxy.js
+++ EnvProxy.js
@@ -66,8 +66,8 @@ class EnvProxy {
             .then(() => this.createProxiedTunnel('consul', 'localhost', 8500))
             .then(() => this.lookupService(this.config['MYSQL_SERVICE']))
             .then(([ip, port]) => this.createProxiedTunnel('mysql', ip, port))
-            .then(() => this.lookupService(this.config['MYSQL_SERVICE_AUTH']))
-            .then(([ip, port]) => this.createProxiedTunnel('mysql_auth', ip, port))
+            //.then(() => this.lookupService(this.config['MYSQL_SERVICE_AUTH']))
+            //.then(([ip, port]) => this.createProxiedTunnel('mysql_auth', ip, port))
             .then(() => this)
             .catch((err) => {
                 log.error("init error: ", err);
