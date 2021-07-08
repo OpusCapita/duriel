096f15880a6a6ba40c20482b711fac49749e0023
Maciej Wakula <maciej.wakula@opuscapita.com>
2021-07-08 09:23:12 +0200

    Enabling tests that seems to work

--- test/TestSuite.js
+++ test/TestSuite.js
@@ -9,18 +9,16 @@ process.env.andariel_loglevel = "warn";
 
 async function fun() {
     describe("Test duriel!", async () => {
-        /*
-        await require('./IntegrationTests').run();
+        //await require('./IntegrationTests').run();
         await require("./BaseFunctionTests").run();
-        await require("./MonitoringTests").run();
+        //await require("./MonitoringTests").run();
         await require("./VersionTests").run();
-        await require("./LibraryTests").run();
-        await require("./CommandBuilderTests").run();
-        await require("./FileHandlingsTests").run();
-        await require("./EnvProxyTests").run();
+        //await require("./LibraryTests").run();
+        //await require("./CommandBuilderTests").run();
+        //await require("./FileHandlingsTests").run();
+        //await require("./EnvProxyTests").run();
         await require("./VariableInjectionTests").run();
-        await require('./ConsulFunctionTests').run();
-        */
+        //await require('./ConsulFunctionTests').run();
     })
 }
 
